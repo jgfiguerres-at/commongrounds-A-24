b25ffdcfd890ebb67cb943cebec4159ad9d81afc
Coby Velasco <carlos.velasco@student.ateneo.edu>
2026-02-28 16:39:54 +0800

    Add genre and book model

--- bookclub/models.py
+++ bookclub/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+    
+
+class Book(models.Model):
+
+    title = models.CharField(max_length=255)
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="books"
+    )
+    author = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-publication_year"]
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
