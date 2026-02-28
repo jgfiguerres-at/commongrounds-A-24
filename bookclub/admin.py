b9b278a09cf93f3e720e64bc881ce0938568bf01
Coby Velasco <carlos.velasco@student.ateneo.edu>
2026-02-28 16:42:46 +0800

    Update admin panels

--- bookclub/admin.py
+++ bookclub/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Genre, Book
 
-# Register your models here.
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "title",
+        "author",
+        "genre",
+        "publication_year",
+        "created_on",
+        "updated_on",
+    )
+    list_filter = ("genre", "publication_year")
+    search_fields = ("title", "author")
