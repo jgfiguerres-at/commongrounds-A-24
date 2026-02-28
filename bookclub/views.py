82cb5f1d55641989eeae028279f364fd7a08ab73
Coby Velasco <carlos.velasco@student.ateneo.edu>
2026-02-28 16:41:01 +0800

    Update booklist and bookdetail view

--- bookclub/views.py
+++ bookclub/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Book
 
-# Create your views here.
+class BookListView(ListView):
+
+    model = Book
+    template_name = "bookclup/book_list.html"
+    context_object_name = "books"
+
+
+class BookDetailView(DetailView):
+
+    model = Book
+    template_name = "bookclub/book_detail.html"
+    context_object_name = "book"
+
+
