b45f0fe911af96e9a5812a5fe8757a30ded20ab6
Marcus Gonzales <marcus.bernard.gonzales@student.ateneo.edu>
2026-03-03 20:23:27 +0800

    Add function-based views for event list and detail

--- localevents/views.py
+++ localevents/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Event
 
-# Create your views here.
+def event_list(request):
+    events = Event.objects.all()
+    ctx = {
+        'events': events,
+    }
+    return render(request, 'localevents/event_list.html', ctx)
+
+def event_detail(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    return render(request, 'localevent/event_detail.html', {
+        'event': event,
+    })
