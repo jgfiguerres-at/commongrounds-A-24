2c2ae5b3488a62d628cb51da59304ed3ebfc5c96
Marcus Gonzales <marcus.bernard.gonzales@student.ateneo.edu>
2026-03-04 19:40:48 +0800

    Align code with PEP8 standards

--- localevents/models.py
+++ localevents/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class EventType(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
@@ -9,6 +10,7 @@ class EventType(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Event(models.Model):
     title = models.CharField(max_length=255)
--- localevents/views.py
+++ localevents/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Event
 
+
 def event_list(request):
     events = Event.objects.all()
     ctx = {
         'events': events,
     }
     return render(request, 'localevents/event_list.html', ctx)
+
 
 def event_detail(request, pk):
     event = get_object_or_404(Event, pk=pk)
