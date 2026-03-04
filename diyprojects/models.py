d58145f91e3624ac487a81a113713a43843f0bcd
jacob-guda06 <jacob.guda@student.ateneo.edu>
2026-03-04 22:53:16 +0800

    Edit models.py

--- diyprojects/models.py
+++ diyprojects/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class ProjectCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('diyprojects:project_list')
+    class Meta:
+        ordering = ['name']
+        verbose_name = "project category"
+        verbose_name_plural = "project categories"
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        ProjectCategory,
+        on_delete=models.SET_NULL,
+        related_name = 'project_list',
+        null=True,
+        blank=True,
+    )
+
+    description = models.TextField()
+    materials = models.TextField()
+    steps = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('project_detail', args=[self.id])
+    
+    class Meta:
+        ordering = ['-created_on']
