1e6c48528d38c69808b1d86485a7143f29e6365f
Justin Edward G. Figuerres <justin.edward.figuerres@student.ateneo.edu>
2026-03-04 19:48:24 +0800

    Add ProductType and Product models

--- merchstore/models.py
+++ merchstore/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class ProductType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'product type'
+        verbose_name_plural = 'product types'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    ProductType
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
