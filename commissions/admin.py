a6fa0df94c6673a31f40f348228145f1c1b0708a
Coby Velasco <carlos.velasco@student.ateneo.edu>
2026-03-05 15:01:12 +0800

    Add commissions app admin panel

--- commissions/admin.py
+++ commissions/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import CommissionType, Commission
 
-# Register your models here.
+
+@admin.register(CommissionType)
+class CommissionTypeAdmin(admin.ModelAdmin):
+
+    list_display = ("name",)
+    search_fields = ('name',)
+
+
+@admin.register(Commission)
+class CommissionAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "title",
+        "people_required",
+        "created_on",
+        "updated_on",
+    )
+    search_fields = ('title',)
+    list_filter = ('created_on',) 
