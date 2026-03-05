57d0c9c9d88cd9ae0f64ca936d16223bbaf43ab7
Marcus Gonzales <marcus.bernard.gonzales@student.ateneo.edu>
2026-03-05 16:09:55 +0800

    Convert function-based views to class-based views

--- localevents/urls.py
+++ localevents/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('events/', event_list, name='event_list'),
-    path('event/<int:pk>/', event_detail, name='event_detail'),
+    path('localevents/events', EventListView.as_view(), name='event_list'),
+    path('localevents/event/<int:pk>', EventDetailView.as_view(), name='event_detail'),
 ]
 
 app_name = 'localevents'
--- localevents/views.py
+++ localevents/views.py
@@ -1,17 +1,16 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+
 from .models import Event
 
 
-def event_list(request):
-    events = Event.objects.all()
-    ctx = {
-        'events': events,
-    }
-    return render(request, 'localevents/event_list.html', ctx)
+class EventListView(ListView):
+    model = Event
+    template_name = 'localevents/event_list.html'
 
 
-def event_detail(request, pk):
-    event = get_object_or_404(Event, pk=pk)
-    return render(request, 'localevent/event_detail.html', {
-        'event': event,
-    })
+class EventDetailView(DetailView):
+    model = Event
+    template_name = 'localevents/event_detail.html'
+
