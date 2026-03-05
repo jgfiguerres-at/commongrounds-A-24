ed511f207db6be8990d7d3b8c7954d20b7682116
Coby Velasco <carlos.velasco@student.ateneo.edu>
2026-03-05 19:21:51 +0800

    Fix urls and align formatting with other apps

--- commissions/urls.py
+++ commissions/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import CommissionListView, CommissionDetailView
 
-
-app_name = 'commissions'
-
+from .views import *
 
 urlpatterns = [
-    path("requests", CommissionListView.as_view(), name="commission_list"),
-    path("request/<int:pk>", CommissionDetailView.as_view(), name="commission_detail"),
+    path("commissions/requests", CommissionListView.as_view(), name="commission_list"),
+    path("commissions/request/<int:pk>", CommissionDetailView.as_view(), name="commission_detail"),
 ]
+
+app_name = 'commissions'
