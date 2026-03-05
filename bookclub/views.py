05865db5d37c9811141b50c072f70f8cc5f72b50
Coby Velasco <carlos.velasco@student.ateneo.edu>
2026-03-05 19:15:06 +0800

    Align formatting with other files

--- bookclub/views.py
+++ bookclub/views.py
@@ -1,17 +1,16 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+
 from .models import Book
 
 
 class BookListView(ListView):
-
     model = Book
     template_name = "bookclub/book_list.html"
     context_object_name = "books"
 
 
 class BookDetailView(DetailView):
-
     model = Book
     template_name = "bookclub/book_detail.html"
     context_object_name = "book"
