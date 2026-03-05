b0557e5ac73763c1598a97495353fe90c888038c
Coby Velasco <carlos.velasco@student.ateneo.edu>
2026-03-05 19:14:50 +0800

    Add fieldsets and align formatting with other apps

--- bookclub/admin.py
+++ bookclub/admin.py
@@ -2,23 +2,37 @@ from django.contrib import admin
 from .models import Genre, Book
 
 
-@admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
-
+    model = Genre
     list_display = ("name",)
     search_fields = ("name",)
 
+    fieldsets = [
+        ("Details", {
+            "fields": [
+                "name",
+                "description",
+            ]
+        })
+    ]
 
-@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-
-    list_display = (
-        "title",
-        "author",
-        "genre",
-        "publication_year",
-        "created_on",
-        "updated_on",
-    )
+    model = Book
+    list_display = ("title", "author", "genre", "publication_year", "created_on", "updated_on",)
     list_filter = ("genre", "publication_year")
     search_fields = ("title", "author")
+
+    fieldsets = [
+        ("Details", {
+            "fields": [
+                "title",
+                "author",
+                "genre",
+                "publication_year",
+            ]
+        }),
+    ]
+
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Book, BookAdmin)
