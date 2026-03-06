65ee5d3e1fa4184865600d253b1a41f904e30f16
Justin Edward G. Figuerres <justin.edward.figuerres@student.ateneo.edu>
2026-03-06 18:44:46 +0800

    Remove unneeded import in views.py

--- merchstore/views.py
+++ merchstore/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 
-from .models import ProductType, Product
+from .models import Product
 
 
 class ItemListView(ListView):
