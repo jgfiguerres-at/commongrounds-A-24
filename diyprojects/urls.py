dbf86638c335b24def52d401f84d2c0814e812a5
jacob-guda06 <jacob.guda@student.ateneo.edu>
2026-03-06 18:29:36 +0800

    Aligned with PEP 8 and fixed html

--- diyprojects/urls.py
+++ diyprojects/urls.py
@@ -5,6 +5,8 @@ from .views import ProjectListView, ProjectDetailView
 app_name = 'diyprojects'
 
 urlpatterns = [
-    path('diyprojects/projects', ProjectListView.as_view(), name='project_list'),
-    path('diyprojects/project/<int:pk>', ProjectDetailView.as_view(), name='project_detail'),
+    path('diyprojects/projects', ProjectListView.as_view(),
+          name='project_list'),
+    path('diyprojects/project/<int:pk>', ProjectDetailView.as_view(), 
+         name='project_detail'),
 ]
