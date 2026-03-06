c3715d10f64e187e1bd814025e578d8950aafce6
Justin Edward G. Figuerres <justin.edward.figuerres@student.ateneo.edu>
2026-03-06 18:52:39 +0800

    Remove indented whitespaces in models.py

--- merchstore/models.py
+++ merchstore/models.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    
+
     def __str__(self):
         return self.name
 
@@ -25,7 +25,7 @@ class Product(models.Model):
     )
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    
+
     def __str__(self):
         return self.name
 
