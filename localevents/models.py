208050cab1c7e1048f6fc43ad4a039dbcc740b95
Marcus Gonzales <marcus.bernard.gonzales@student.ateneo.edu>
2026-03-06 13:36:38 +0800

    Include base html implementation for event detail and list views

--- localevents/models.py
+++ localevents/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class EventType(models.Model):
@@ -37,3 +38,6 @@ class Event(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('localevents:event_detail', args=[int(self.pk)])
