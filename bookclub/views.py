abef7558cf7505d4ff6ee005cb4803663001373b
jgfiguerres-at <justin.edward.figuerres@student.ateneo.edu>
2026-05-08 23:32:19 +0800

    Fix role permissions in bookclub/views.py

--- bookclub/views.py
+++ bookclub/views.py
@@ -14,7 +14,7 @@ def is_book_contributor(user):
     return ( 
         user.is_authenticated and 
         hasattr(user, "profile") and 
-        user.groups.filter(name="Commission Maker").exists() 
+        user.groups.filter(name="Book Contributor").exists() 
     )
 
 
